4f19704e0ff2558e0d0ae497b701fd390ff88ad5
MansiKapadia <mansikapadia2466@gmail.com>
2024-08-08 12:43:33 +0530

    first commit

---
+++ src/BlackJack.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class BlackJack {
+    private class Card {
+        String value;
+        String type;
+
+        Card(String value, String type) {
+            this.value = value;
+            this.type = type;
+        }
+
+        public String toString() {
+            return value + "-" + type;
+        }
+    }
+
+    ArrayList<Card> deck;
+    Random random=new Random();//shuffle deck
+
+    BlackJack() {
+        startGame();
+    }
+
+    public void startGame() {
+        buildDeck();
+        shuffleDeck();
+    }
+
+    public void buildDeck() {
+        deck = new ArrayList<Card>();
+        String[] values = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        String[] types = {"C", "D", "H", "S"};
+
+        for (int i = 0; i < types.length; i++) {
+            for (int j = 0; j < values.length; j++) {
+                Card card = new Card(values[j], types[i]);
+                deck.add(card);
+            }
+        }
+        System.out.println("BUILD DECK:");
+        System.out.println(deck);
+    }
+
+    public void shuffleDeck()
+    {
+        for(int i=0;i<deck.size();i++)
+        {
+            int j=random.nextInt(deck.size());
+            Card currCard=deck.get(i);
+            Card randomCard=deck.get(j);
+            deck.set(i,randomCard);
+            deck.set(j,currCard);
+            
+        }
+        System.out.println("AFTER SHUFFLE");
+        System.out.println(deck);
+
+    }
+    public static void main(String[] args) {
+        BlackJack game = new BlackJack();
+    }
+}
